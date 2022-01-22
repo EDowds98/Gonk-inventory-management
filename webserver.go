ae681888bd4275dd57a55612949b0383043072f7
Eric Dowds <ericdowds1998@gmail.com>
2022-01-22 11:52:54 +0000

    working on adding login page

--- webserver.go
+++ webserver.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func GetIndexHandler(w http.ResponseWriter, r *http.Request) {
+	p := ("./website/index.html")
+	// set header
+	w.Header().Set("Content-type", "text/html")
+	http.ServeFile(w, r, p)
+
+}
+
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+		p := ("./website/portal.html")
+		http.ServeFile(w, r, p)
+
+	case "POST":
+		uname := r.FormValue("uname")
+		pwd := r.FormValue("pwd")
+		redirectTarget := "/"
+
+		if uname == "admin" && pwd == "root" {
+			redirectTarget = "http://www.google.com"
+			http.Redirect(w, r, redirectTarget, http.StatusMovedPermanently)
+		} else {
+			http.Redirect(w, r, redirectTarget, http.StatusNotFound)
+		}
+	}
+}
+
 func main() {
-	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("website"))
-	mux.Handle("/", fs)
-	http.ListenAndServe(":8080", mux)
+
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/", GetIndexHandler).Methods("GET")
+	r.HandleFunc("/portal.html", FormHandler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	server.ListenAndServe()
 }
