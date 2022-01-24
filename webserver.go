da28c7a300b7493ce1e7dc37b0e8a20f79814848
Eric Dowds <ericdowds1998@gmail.com>
2022-01-24 16:29:24 +0000

    fixed css, broke redirect

--- webserver.go
+++ webserver.go
@@ -11,36 +11,36 @@ func GetIndexHandler(w http.ResponseWriter, r *http.Request) {
 	// set header
 	w.Header().Set("Content-type", "text/html")
 	http.ServeFile(w, r, p)
-
 }
 
 func FormHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
-	case "GET":
-		p := ("./website/portal.html")
-		http.ServeFile(w, r, p)
 
 	case "POST":
 		uname := r.FormValue("uname")
 		pwd := r.FormValue("pwd")
-		redirectTarget := "/"
 
 		if uname == "admin" && pwd == "root" {
-			redirectTarget = "http://www.google.com"
-			http.Redirect(w, r, redirectTarget, http.StatusMovedPermanently)
+			http.Redirect(w, r, "https://www.google.com", http.StatusFound)
 		} else {
-			http.Redirect(w, r, redirectTarget, http.StatusNotFound)
+			http.Redirect(w, r, "https://wikipedia.com", http.StatusFound)
 		}
+
+	case "GET":
+		p := ("./website/portal.html")
+		http.ServeFile(w, r, p)
 	}
 }
 
 func main() {
 
 	r := mux.NewRouter().StrictSlash(false)
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./website/")))
+
 	r.HandleFunc("/", GetIndexHandler).Methods("GET")
 	r.HandleFunc("/portal.html", FormHandler)
-
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: r,
