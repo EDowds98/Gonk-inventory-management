13b08c49c236218097c963d6358bd394388aa9a8
EDowds98 <ericdowds1998@gmail.com>
2022-03-13 20:43:11 +0000

    made it more human readable

--- js/table-update.js
+++ js/table-update.js
@@ -38,7 +38,11 @@ function updateTable() {
     for (let i = 0; i < arrays.length; i++) {
         let innerArrayLength = arrays[i].length;
         for (let j = 0; j < innerArrayLength; j++) {
-            table.rows[i + 1].cells[j + 1].innerHTML = arrays[i][j];
+            if(arrays[i][j]) {
+                table.rows[i + 1].cells[j + 1].innerHTML = "full! 😄";
+            } else {
+                table.rows[i + 1].cells[j + 1].innerHTML = "empty! 😞";
+            }
         }
     }
 }
